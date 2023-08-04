25666f2d47423f1d4d95e6e3411956532c3c31be
dark7nights <alalimohammed258@gmail.com>
2023-08-04 18:42:25 +0300

    bot color problem

--- bot_🤖/pil/text.py
+++ bot_🤖/pil/text.py
@@ -1,42 +1,37 @@
 from PIL import Image, ImageDraw, ImageFont
 
-quote = "Don't think of yourself as a woman in business"
-print(len(quote))
-if 58 <= len(quote):
-    slashed = 45
+def add_new_lines(string: str, slashed: int) -> str:
+    if len(string) <= slashed:
+        return string
     s = []
-    for char in quote:
+    for char in string:
        s.append(char)
     for char in s[slashed:]:
         try:
             n = s[slashed:].index(' ')
-        except:
+        except Exception:
             continue
         s[n+slashed] = '\n'
-        new_s = ''.join(s)
-        slashed+=45
+        slashed+=slashed
         if slashed > len(s):
-            break
-print(new_s)
+            return ''.join(s)
 
-with Image.open('../modified.jpeg') as img:
-    draw = ImageDraw.Draw(img)
-
-    text = new_s
+def draw(image_path: str, saved_image: str, format: str, quote: str, auther: str, font_color: str='black') -> None:
+    with Image.open(image_path) as img:
+        draw = ImageDraw.Draw(img)
 
     fnt = ImageFont.truetype("../font/static/Raleway-Bold.ttf", 80)
 
     # get the middle dimensions for main
-    left, top, right, bottom = draw.textbbox((0,0), text, font=fnt)
+    left, top, right, bottom = draw.textbbox((0,0), text=quote, font=fnt)
     quote_width = right - left
     quote_height = bottom - top
-    
-    
+
     center_x_q = (img.size[0] - quote_width) / 2
     center_y_q = (img.size[1] - quote_height) / 2
 
     # drawing the text
-    draw.multiline_text((center_x_q, center_y_q - 100), text=text, fill='white', font=fnt, align='center')
+    draw.multiline_text((center_x_q, center_y_q - 150), text=quote, fill=font_color, font=fnt, align='center')
 
     # get the middle dimension for auther
     left, top, right, bottom = draw.textbbox((0,0), auther, font=fnt)
@@ -45,14 +40,19 @@ with Image.open('../modified.jpeg') as img:
 
     center_x_a = (img.size[0] - auther_width) / 2
     center_y_a = (img.size[1] - auther_height) / 2
-    
+
     # drawing the text
-    # draw.text((center_x_a, center_y_a), text=auther, fill='white', font=fnt, align='center')
     y = center_y_a - center_y_q
-    draw.text((center_x_a, center_y_a + y), text=auther, fill='white', font=fnt, align='center')
+    draw.text((center_x_a, center_y_a + y), text=f'"{auther}"', fill=font_color, font=fnt, align='center')
 
-    print(f'Coordinates for quote: x:{center_x_q}, y:{center_y_q}\nfor auther: x:{center_x_a} y:{center_y_a}\n the difference: y: {y}')
+    img.save(saved_image, format.upper())
 
-    
+quote = "Don't think of yourself as a woman in business, Think of yourself as man."
+author = 'Hello ladies'
+x = add_new_lines(quote, 40)
+media_path_pre = '../images/premod.jpg'
+media_path_post = 'postmod.webp'
 
-    img.save('text.webp', 'WEBP')
+draw(media_path_pre, media_path_post, 'WEBP', x, author)
+print(len(quote))
+print(x)
